020661776c542a4fdb926dab588d20fcbb01b1b3
Army Nougues <armyangray@gmail.com>
2023-06-02 13:20:59 +0200

    📦 NEW: added some hooks

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as useWindowSize } from "./hooks/useWindowSize";
+export { default as useInterval } from "./hooks/useInterval";
+export { default as useFetch } from "./hooks/useFetch";
+export { default as useTimeout } from "./hooks/useTimeout";
+export { default as usePersistedState } from "./hooks/usePersistedState";
